aabbf462c48b0ad993ba2d2134a931beb05e6107
Jason Cai <jasoncai528@gmail.com>
2025-03-08 21:06:33 -0700

    create 2048 lib

---
+++ libs/games/game-2048/src/lib/game-2048.tsx
@@ -0,0 +1,11 @@
+import styles from './game-2048.module.scss';
+
+export function Game2048() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Game2048!</h1>
+    </div>
+  );
+}
+
+export default Game2048;
