853c3403017cd993c8f5e099657aae8e9c33d4ab
Jason Cai <jasoncai528@gmail.com>
2025-03-10 19:30:18 -0700

    environment config setup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,11 +2,15 @@
 // import styles from './app.module.css';
 import { MantineProvider } from '@mantine/core';
 import AppMainShell from './components/app-main-shell/app-main-shell';
+import { EnvironmentContext } from '@JC/shared/context';
+import { environment } from './environment-config';
 
 export function App() {
   return (
     <MantineProvider>
-      <AppMainShell />
+      <EnvironmentContext.Provider value={environment}>
+        <AppMainShell />
+      </EnvironmentContext.Provider>
     </MantineProvider>
   );
 }
