88577a076250bdd9054944cf3f76523cd3cd2641
Jason Cai <jasoncai528@gmail.com>
2025-04-09 15:34:13 -0700

    enable copy to clipboard for code blocks

--- libs/tutorials/src/lib/markdown-renderer.tsx
+++ libs/tutorials/src/lib/markdown-renderer.tsx
@@ -8,6 +8,8 @@ import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json';
 import yaml from 'react-syntax-highlighter/dist/esm/languages/hljs/yaml';
 import csharp from 'react-syntax-highlighter/dist/esm/languages/hljs/csharp';
 import ts from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+import { ActionIcon, Tooltip } from '@mantine/core';
+import { IconClipboard, IconCopy } from '@tabler/icons-react';
 
 export function MarkdownRenderer({ markdown }: { markdown: string }) {
   SyntaxHighlighter.registerLanguage('javascript', js);
@@ -20,22 +22,51 @@ export function MarkdownRenderer({ markdown }: { markdown: string }) {
   SyntaxHighlighter.registerLanguage('c#', csharp);
   SyntaxHighlighter.registerLanguage('ts', ts);
 
+  const handleCopy = (code: string) => {
+    navigator.clipboard
+      .writeText(code)
+      .then(() => {
+        console.log('Copied to clipboard!');
+      })
+      .catch((err) => {
+        console.error('Failed to copy text: ', err);
+      });
+  };
+
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
       components={{
         code({ node, inline, className, children, ...props }: any) {
           const match = /language-(\w+)/.exec(className || '');
+          const code = String(children).replace(/\n$/, '');
 
           return !inline && match ? (
-            <SyntaxHighlighter
-              style={nightOwl}
-              PreTag="div"
-              language={match[1]}
-              {...props}
-            >
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
+            <div style={{ position: 'relative' }}>
+              <Tooltip label="Copy to clipboard">
+                <ActionIcon
+                  style={{
+                    position: 'absolute',
+                    left: '100%',
+                    transform: 'translateX(-100%)',
+                  }}
+                  size="xl"
+                  radius="xl"
+                  variant="transparent"
+                  onClick={() => handleCopy(code)}
+                >
+                  <IconCopy size={24} />
+                </ActionIcon>
+              </Tooltip>
+              <SyntaxHighlighter
+                style={nightOwl}
+                PreTag="div"
+                language={match[1]}
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            </div>
           ) : (
             <code className={className} {...props}>
               {children}
